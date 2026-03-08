0443236048abefdcc0c907fb5809ae11529d7137
Sapper <saniamykhailenko@gmail.com>
2026-03-08 13:00:22 +0200

    add: Footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,8 +3,14 @@ import css from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={css.footer}>
-      <div className="container">
-        <h2>Footer</h2>
+      <div className={css.wrap}>
+        <p>Developer: Oleksandr Mykhailenko</p>
+        <p>
+          Contact us:&nbsp;
+          <a href="mailto:saniamykhailenko@gmail.com">
+            saniamykhailenko@gmail.com
+          </a>
+        </p>
       </div>
     </footer>
   );
